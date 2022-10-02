590528d93d9b5ba8f1af2f878bcf195f4e817b96
Eeses383 <111698237+Eeses383@users.noreply.github.com>
2022-10-02 03:36:20 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+channel_id = -1001530420320  #id канала (для уведомлений)
+
+parner_channel = -1001639401950  # id канала для выплат партнерам
+
+admin = 1943146473  #id админстратора (для выплат)
+
+chats = -1001639401950 #id чата
+
+link_chat = 'https://t.me/HackSquadLINK' #Ссылка на чат
+
+token = '5531668957:AAEwZYFak7QsUvJBAymhdpJ3WL1PSM1p8q0' #токен бота
+
+username_bot = 'https://t.me/HackSquadLINK'
+
+news_link = 'https://t.me/HackSquadLINK'
+
+admin_link = 'https://t.me/HackSquadLINK'
